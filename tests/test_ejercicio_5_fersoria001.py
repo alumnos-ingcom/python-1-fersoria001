f1765740d8daf2fb0047ab7562092005caa5450a
bercho001 <bercho001@gmail.com>
2022-05-30 13:04:16 -0300

    Finalizado test de ejercicio 5

--- tests/test_ejercicio_5_fersoria001.py
+++ tests/test_ejercicio_5_fersoria001.py
@@ -22,3 +22,61 @@ def test_division_lenta_positivos():
     assert division[0] == 15//2, "El resultado no es correcto"
     assert division[1] == 15%2, "El resultado no es correcto"
     assert division == (7,1), "El resultado no es correcto"
+
+
+def test_division_lenta_negativo_con_positivo():
+    """ Esta funcion se encarga de testear la funcion division lenta del
+    ejercicio5 con dividendo negativo y divisor positivo
+    """
+    dividendo = -15
+    divisor = 2
+    division = division_lenta(dividendo,divisor)
+    assert isinstance(division, tuple), "Debe retornar una tupla con cociente y resto"
+    assert division[0] == -15//2, "El resultado no es correcto"
+    assert division[1] == -15%2, "El resultado no es correcto"
+    assert division[1] > 0, "el resto debe ser positivo"
+    assert division[1] < abs(dividendo), "el resto debe ser menor al modulo del dividendo"
+    assert division == (-8,1), "El resultado no es correcto"
+
+
+def test_division_lenta_positivo_con_negativo():
+    """ Esta funcion se encarga de testear la funcion division lenta del
+    ejercicio5 con dividendo positivo y divisor negativo
+    """
+    dividendo = 15
+    divisor = -2
+    division = division_lenta(dividendo,divisor)
+    assert isinstance(division, tuple), "Debe retornar una tupla con cociente y resto"
+    assert division[0] == 15//(-2), "el cociente es incorrecto"
+    assert division[1] == 15%(-2), "el resto es incorrecto"
+    assert division[1] < abs(dividendo), "el resto debe ser menor al modulo del dividendo"
+    assert division == (-8,-1), "el resultado no es correcto"
+
+
+def test_division_lenta_negativo_con_positivo():
+    """ Esta funcion se encarga de testear la funcion division lenta del
+    ejercicio5 con dividendo positivo y divisor negativo
+    """
+    dividendo = -15
+    divisor = 2
+    division = division_lenta(dividendo,divisor)
+    assert isinstance(division, tuple), "Debe retornar una tupla con cociente y resto"
+    assert division[0] == -15//2, "el cociente no es correcto"
+    assert division[1] == -15%2, "el resto no es correcto"
+    assert division[1] > 0, "el resto debe ser positivo"
+    assert division[1] < abs(dividendo), "el resto debe ser menor al modulo del dividendo"
+    assert division == (-8,1), "el resultado no es correcto"
+
+
+def test_division_lenta_negativos():
+    """ Esta funcion se encarga de testear la funcion division lenta del
+    ejercicio5 con dividendo positivo y divisor negativo
+    """
+    dividendo = -15
+    divisor = -2
+    division = division_lenta(dividendo,divisor)
+    assert isinstance(division, tuple), "Debe retornar una tupla con cociente y resto"
+    assert division[0] == -15//(-2), "el cociente no es correcto"
+    assert division[1] == -15%(-2), "el resto no es correcto"
+    assert division[1] < abs(dividendo), "el resto debe ser menor al modulo del dividendo"
+    assert division == (7,-1), "el resultado no es correcto"
