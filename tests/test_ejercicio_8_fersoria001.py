2b6046cc364871d0ca6bcb4f039882bedad54b8d
bercho001 <bercho001@gmail.com>
2022-05-31 20:58:22 -0300

    Agregado caso con numero negativo al test de la funcion es_primo

--- tests/test_ejercicio_8_fersoria001.py
+++ tests/test_ejercicio_8_fersoria001.py
@@ -26,11 +26,21 @@ def test_es_primo():
 
 def test_es_primo_no():
     """ Esta funcion se encarga de testear la funcion que indica
-    si un numero es primo o no en el ejercicio8 con valor negativo
+    si un numero es primo o no en el ejercicio8 con valor False
     """
     numero = 36
     resultado = es_primo(numero)
     assert isinstance(resultado, bool), "El resultado debe ser True o False"
     assert numero%2 == 0 and resultado is False, "Resultado incorrecto"
     assert numero%3 == 0 and resultado is False, "Resultado incorrecto"
+    assert resultado is False, "Resultado incorrecto"
+
+def test_es_primo_negativo():
+    """ Esta funcion se encarga de testear la funcion que indica
+    si un numero negativo
+    es primo o no en el ejercicio8
+    """
+    numero = -7
+    resultado = es_primo(numero)
+    assert isinstance(resultado, bool), "El resultado debe ser True o False"
     assert resultado is False, "Resultado incorrecto"
